5eeac22841ca3db63497813cb4f273272b300e49
RealTun <maumai07@gmail.com>
2025-01-10 00:19:44 +0700

    api job, predict to ui, update api openai and job, change endpoint role

--- backend/src/controllers/job.controller.js
+++ backend/src/controllers/job.controller.js
@@ -1,17 +1,16 @@
 const axios = require('axios');
 const { exec } = require('child_process');
 const path = require('path');
+const { findUserByUid } = require('../models/repositories/user.repo');
+const { getContentAI } = require('./openai.controller');
 
 // const baseUrl = 'https://ms.vietnamworks.com/job-search/v1.0/search';
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
-const searchJobs = async (req, res) => {
+const getJobsResponse = async (keyword, limit, page) => {
     try {
         const apiUrl = `https://ms.vietnamworks.com/job-search/v1.0/search`;
-
-        const { keyword, limit, page } = req.body;
-        // console.log(req.body);
 
         const bodyToSendRequest = {
             "userId": 0,
@@ -81,19 +80,37 @@ const searchJobs = async (req, res) => {
 
         const response = await axios.post(apiUrl, bodyToSendRequest);
         const data = response.data['data'];
-        const selectedKeys = ['jobTitle', 'jobUrl', 'companyName', 'companyLogo', 'jobDescription', 'jobRequirement', 'skills', 'benefits', 'workingLocations', 'address', 'jobLevel', 'prettySalary', 'rangeAge', 'salaryCurrency'];
+        // const selectedKeys = ['jobTitle', 'jobUrl', 'companyName', 'companyLogo', 'jobDescription', 'jobRequirement', 'skills', 'benefits', 'workingLocations', 'address', 'jobLevel', 'prettySalary', 'rangeAge', 'salaryCurrency'];
+        const selectedKeys = ['jobTitle', 'jobUrl', 'companyName', 'companyLogo', 'workingLocations', 'address', 'jobLevel', 'prettySalary', 'rangeAge', 'salaryCurrency'];
 
         const filteredData = data.map(item => {
             const newObj = {};
             selectedKeys.forEach(key => {
-                newObj[key] = item[key];
+                if (key === 'workingLocations') {
+                    const cityAddress = item.workingLocations.find(location => location.cityId === 24)?.address;
+                    newObj[key] = cityAddress || null; // Nếu không tìm thấy, trả về null
+                } else {
+                    newObj[key] = item[key];
+                }
             });
             return newObj;
         });
 
+        return filteredData;
+    } catch (error) {
+        return null;
+    }
+}
+
+const searchJobs = async (req, res) => {
+    try {
+        const { keyword, limit, page } = req.body;
+
+        const results = await getJobsResponse(keyword, limit, page);
+
         res.status(200).json({
             message: 'Get list jobs success',
-            data: filteredData
+            data: results
         });
     } catch (error) {
         res.status(error.response?.status || 500).json({
@@ -142,40 +159,65 @@ const findCompanyByName = async (req, res) => {
     }
 };
 
+// const predictCareer = async (req, res) => {
+//     try {
+//         // example body
+//         // {
+//         //     "age": 22,
+//         //     "gpa": 3.5,
+//         //     "domain": "Artificial Intelligence",
+//         //     "projects": "Web Development",
+//         //     "python": 3,
+//         //     "sql": 3,
+//         //     "java": 2
+//         // }
+//         const body = req.body;
+
+//         // Đường dẫn tới file Python
+//         const pythonFilePath = path.resolve(__dirname, '../../predictor/final.py');
+
+//         // Tạo command để chạy script Python
+//         const pythonCommand = `python ${pythonFilePath} --age ${body.age} --gpa ${body.gpa} --domain "${body.domain}" --projects "${body.projects}" --python ${body.python} --sql ${body.sql} --java ${body.java}`;
+
+//         // console.log(pythonCommand);
+
+//         // Chạy lệnh Python
+//         exec(pythonCommand, (error, stdout, stderr) => {
+//             if (error) {
+//                 // console.error(`Error: ${error.message}`);
+//                 return res.status(503).json({ message: 'Python script error', error: error.message });
+//             }
+//             if (stderr) {
+//                 // console.error(`Stderr: ${stderr}`);
+//                 return res.status(503).json({ message: 'Python script stderr', error: stderr });
+//             }
+//             // console.log(`Output: ${stdout}`);
+//             return res.status(200).json({ message: 'Predict career success', data: stdout });
+//         });
+//     } catch (error) {
+//         res.status(error.response?.status || 500).json({
+//             message: error.message
+//         });
+//     }
+// };
+
 const predictCareer = async (req, res) => {
     try {
-        // example body
-        // {
-        //     "age": 22,
-        //     "gpa": 3.5,
-        //     "domain": "Artificial Intelligence",
-        //     "projects": "Web Development",
-        //     "python": 3,
-        //     "sql": 3,
-        //     "java": 2
-        // }
-        const body = req.body;
+        const userFound = await findUserByUid(req.user.username);
 
-        // Đường dẫn tới file Python
-        const pythonFilePath = path.resolve(__dirname, '../../predictor/final.py');
+        if (!userFound || userFound.isSynced == null || !userFound.isSynced) {
+            return res.status(400).json({
+                message: 'User is not sync data',
+            });
+        }
 
-        // Tạo command để chạy script Python
-        const pythonCommand = `python ${pythonFilePath} --age ${body.age} --gpa ${body.gpa} --domain "${body.domain}" --projects "${body.projects}" --python ${body.python} --sql ${body.sql} --java ${body.java}`;
+        const promt = `Dựa vào danh sách điểm ngành thuộc công nghệ thông tin của tôi hãy gợi ý ra công việc phù hợp cho tôi ${userFound.list_mark}, chỉ trả lời ra 1 tên công việc phù hợp nhất, không trả lời kèm từ ngữ khác và không có dấu chấm cuối câu`;
 
-        // console.log(pythonCommand);
+        const result = await getContentAI(promt);
 
-        // Chạy lệnh Python
-        exec(pythonCommand, (error, stdout, stderr) => {
-            if (error) {
-                // console.error(`Error: ${error.message}`);
-                return res.status(503).json({ message: 'Python script error', error: error.message });
-            }
-            if (stderr) {
-                // console.error(`Stderr: ${stderr}`);
-                return res.status(503).json({ message: 'Python script stderr', error: stderr });
-            }
-            // console.log(`Output: ${stdout}`);
-            return res.status(200).json({ message: 'Predict career success', data: stdout });
+        res.status(200).json({
+            message: 'Predict career successful',
+            data: result
         });
     } catch (error) {
         res.status(error.response?.status || 500).json({
